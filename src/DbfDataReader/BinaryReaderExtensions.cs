bb7367cf3cbab982b215d34b75be0293487efc2f
DataGrafikk <22612392+DataGrafikk@users.noreply.github.com>
2021-03-11 17:43:32 +0100

    Update BinaryReaderExtensions.cs

--- src/DbfDataReader/BinaryReaderExtensions.cs
+++ src/DbfDataReader/BinaryReaderExtensions.cs
@@ -38,7 +38,7 @@ namespace DbfDataReader
         public static string ReadString(this BinaryReader binaryReader, int fieldLength, Encoding encoding)
         {
             var chars = binaryReader.ReadBytes(fieldLength);
-            if (chars.Length == 0)
+            if ((chars == null) || (chars.Length == 0))
             {
                 return null;
             }
